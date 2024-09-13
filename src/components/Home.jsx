e4e6072f8a8414950df0f024bc75cfedadc23056
Dragolex01 <alexayusoherranz2001@gmail.com>
2024-09-13 20:58:10 +0200

    added SASS to proyect

---
+++ src/components/Home.jsx
@@ -0,0 +1,13 @@
+const Home = () => {
+    return(
+        <section>
+            <div>
+                <h1>
+                    Hola! Soy <span className='text-[#915EFF]'>Álex</span>
+                </h1>
+            </div>
+        </section>
+    )
+}
+
+export default Home;
